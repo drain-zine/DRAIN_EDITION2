68a2ec8207318f79c993b3208807d46aeca31e6b
MPratley <marcuspratley@gmail.com>
2021-02-07 17:16:07 +0000

    example 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("res")
+  res_locations = ["./res", "./StarterPub/res", "./Noticeboard/res"]
+
+  resLocations.map((location) => {
+    eleventyConfig.addPassthroughCopy(location)
+  })
 
   configObject = {
     dir: {
-      input: "views",
-      output: "_site",
-      includes: "_includes", // This path is relative to the input dir
+      // input: "views", // default `.`
+      output: "_site", // default `_site`
+      includes: "_includes", // This path is relative to the input dir. default `_includes`
     },
   }
 
