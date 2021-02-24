bcf061e8ff8150e88d815faff0c58c9ae80227b9
drain-zine <58533733+drain-zine@users.noreply.github.com>
2021-02-24 20:05:24 +0000

    Finished flex conversion for review (see comments) (#32)
    * Finished flex conversion for review (see comments)
    
    * Converted Orcs playing dice to markdown, including random dice roll r… (#35)
    
    * Converted Orcs playing dice to markdown, including random dice roll responses
    
    * re-organising directory structure 4 md
    
    * fixed variables + utilities inheritence
    
    Co-authored-by: Based InvisibleTree <invisibletree@protonmail.ch>
    
    * remove unneeded markdown dep
    
    * began conversion to tailwind + md texture select
    
    * Fixed tailwind int- now good to finish conversion!
    
    * Fixed Headerscroll png so now scroll is centered
    
    * crop image for truer centering
    
    * cleaned up DirtyHack class
    
    * Finsihed Base Styling of Orcs
    
    * Partialisation of StarterPub + Full demo on orcsPlayingDicee (addressing review of PR of orcs-playing-dice) (#37)
    
    * First stage - added root + header partials
    
    * Text box partialising - text not showing :(
    
    * Partialisation + conversion done! + styling update
    
    Co-authored-by: Based InvisibleTree <invisibletree@protonmail.ch>
    
    * Update orcsPlayingDice.njk
    
    Something funky changed the file name
    
    Co-authored-by: Based InvisibleTree <invisibletree@protonmail.ch>
    Co-authored-by: Marcus Pratley <marcuspratley@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,20 @@
+const markdown = require("./utils/markdown")
+const shortcodes = require("./utils/shortcodes")
+
 module.exports = function (eleventyConfig) {
+  // Allow custom markdown parser, switchfrom default engine (liquid) to njk
+  eleventyConfig.setLibrary("md", markdown)
+
+  // Shortcodes
+  eleventyConfig.addPairedShortcode("markdown", shortcodes.markdown)
+
+  // Pass throughs
   copyDirectories = ["res", "LinkedLounge", "ARCHIVE"]
   copyDirectories.map((path) => {
     eleventyConfig.addPassthroughCopy(path)
   })
+
+  eleventyConfig.addWatchTarget("styles")
 
   configObject = {
     dir: {
@@ -11,6 +23,10 @@ module.exports = function (eleventyConfig) {
       includes: "_includes", // This path is relative to the input dir. default `_includes`
       data: "_data", // defaults to _data, relative to input
     },
+
+    // switch default engines from liquid to njk
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   }
 
   return configObject
