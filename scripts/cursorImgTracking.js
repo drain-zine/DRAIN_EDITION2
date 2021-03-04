626ded580b4958c7e36fb93c4f325bd5e49abea5
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-04 19:43:43 +0000

    Fixed Mouse Tracking

--- scripts/cursorImgTracking.js
+++ scripts/cursorImgTracking.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(document).mousemove(function (e) {
-    $("cursorImg").css("left", e.pageX + "px")
-    $("cursorImg").css("top", e.pageY + "px")
+    $("#cursorImg").css("left", e.pageX + "px")
+    $("#cursorImg").css("top", e.pageY + "px")
   })
 })
