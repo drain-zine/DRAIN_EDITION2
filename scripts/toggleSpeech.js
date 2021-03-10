11a537b5611672ad801009f9ad7607766a3d10d2
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-10 14:03:58 +0000

    riddles implementation basic - see hack

--- scripts/toggleSpeech.js
+++ scripts/toggleSpeech.js
@@ -1,5 +1,12 @@
-export function toggleSpeech(speechText, style) {
+export function toggleSpeech(speechText, fadeIn = 1000, fadeOut = 3000) {
   var textbox = document.getElementById("speechText")
-  $(textbox).text(speechText[Math.floor(Math.random() * speechText.length)])
-  document.getElementById("speechContainer").style.display = style
+  $(textbox).text(speechText)
+
+  setTimeout(function () {
+    document.getElementById("speechContainer").style.display = "block"
+  }, fadeIn)
+
+  setTimeout(function () {
+    document.getElementById("speechContainer").style.display = "none"
+  }, fadeOut)
 }
