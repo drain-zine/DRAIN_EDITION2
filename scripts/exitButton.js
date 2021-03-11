f4da4039371b914b96d5af9ecee1a8f5aa416407
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-11 18:23:21 +0000

    Mass rename

--- scripts/exitButton.js
+++ scripts/exitButton.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
   $("#exitButton").mouseover(function () {
     console.log("hover!")
-    $(this).delay(1000).attr("src", "/res/StarterPub/dynamic/cheers.gif")
+    $(this).delay(1000).attr("src", "/res/YeOldeFountain/dynamic/cheers.gif")
   })
 
   $("#exitButton").mouseout(function () {
-    $(this).delay(1000).attr("src", "/res/StarterPub/dynamic/cheersStill.gif")
+    $(this)
+      .delay(1000)
+      .attr("src", "/res/YeOldeFountain/dynamic/cheersStill.gif")
   })
 
   $("#exitButton").click(function () {
-    const audio = new Audio("/res/StarterPub/sound/cheers.mp3")
+    const audio = new Audio("/res/YeOldeFountain/sound/cheers.mp3")
     audio.play()
     // $("#introBodyDiv").fadeOut(6000);
     setTimeout("window.open('welcome.html', '_self')", 2000)
