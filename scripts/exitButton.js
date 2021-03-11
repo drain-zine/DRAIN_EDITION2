44e977faa1f8a2dd963a8cdb1e2a06894805d24a
Based InvisibleTree <invisibletree@protonmail.ch>
2021-03-11 12:51:01 +0000

    cleanup

--- scripts/exitButton.js
+++ scripts/exitButton.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $("#exitButton").mouseover(function () {
+    console.log("hover!")
     $(this).delay(1000).attr("src", "/res/StarterPub/dynamic/cheers.gif")
   })
 
@@ -11,6 +12,6 @@ $(document).ready(function () {
     const audio = new Audio("/res/StarterPub/sound/cheers.mp3")
     audio.play()
     // $("#introBodyDiv").fadeOut(6000);
-    setTimeout("window.open('welcomeTraveller.html', '_self')", 2000)
+    setTimeout("window.open('welcome.html', '_self')", 2000)
   })
 })
